79cb497906ad6f0743cc6365e63ad7d1547e9c8a
LeoZ213 <113804442+LeoZ213@users.noreply.github.com>
2022-12-31 14:30:28 -0500

    Add files via upload

---
+++ part11-Part11_03.ShowAndTicket/src/main/java/Show.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+public class Show {
+    private String movie;
+    private String time;
+}
---
+++ part11-Part11_03.ShowAndTicket/src/main/java/Ticket.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+public class Ticket {
+    private int seat;
+    private int code;
+    private Show show;
+}
